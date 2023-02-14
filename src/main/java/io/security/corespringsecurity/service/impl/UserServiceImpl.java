c94675875aef78a0212de2a477c26314f168c4d4
woohyeonjoe <qlqkthd8802@gmail.com>
2023-02-14 16:47:18 +0900

    UserServiceImpl 생성

---
+++ src/main/java/io/security/corespringsecurity/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package io.security.corespringsecurity.service.impl;
+
+import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void createUser(Account account) {
+
+    }
+}
